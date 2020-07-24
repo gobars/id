b4cca5efdcc193b10f0d2c3b163b36249e03d952
bingoohuang <bingoo.huang@gmail.com>
2020-07-24 15:45:07 +0800

    try lock worker id to keep unique in the same host.

--- src/main/java/com/github/gobars/id/util/Files.java
+++ src/main/java/com/github/gobars/id/util/Files.java
@@ -1,15 +1,53 @@
 package com.github.gobars.id.util;
 
 import lombok.Cleanup;
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
 import java.io.*;
+import java.nio.channels.OverlappingFileLockException;
 
 @UtilityClass
 @Slf4j
 public class Files {
+  public final String GOBARS_ID = System.getProperty("user.home") + File.separator + ".gobars_id";
+
+  static {
+    File dir = new File(GOBARS_ID);
+    dir.mkdirs();
+    if (!dir.exists()) {
+      throw new RuntimeException("create dirs ~/.gobarsid failed");
+    }
+  }
+
+  public int findAvailableWorkerID(int workerID) {
+    if (lockWorkerID(workerID)) {
+      return workerID;
+    }
+
+    for (int i = workerID + 100, j = 0; j < 1000; i++) {
+      if (lockWorkerID(i)) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  public boolean lockWorkerID(int workerID) {
+    return tryLockFile(GOBARS_ID + File.separator + "workerID." + workerID);
+  }
+
+  @SneakyThrows
+  public boolean tryLockFile(String filename) {
+    try {
+      return new RandomAccessFile(filename, "rw").getChannel().tryLock() != null;
+    } catch (OverlappingFileLockException e) {
+      return false;
+    }
+  }
 
   public String readFile(String filePath) throws IOException {
     @Cleanup val dis = new DataInputStream(new FileInputStream(filePath));
---
+++ src/test/java/com/github/gobars/id/util/FilesTest.java
@@ -0,0 +1,14 @@
+package com.github.gobars.id.util;
+
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class FilesTest {
+    @Test
+    public void findAvailableWorkerID() {
+        assertThat(Files.findAvailableWorkerID(1)).isEqualTo(1);
+        assertThat(Files.findAvailableWorkerID(1)).isGreaterThan(100);
+        assertThat(Files.findAvailableWorkerID(1)).isGreaterThan(101);
+    }
+}
