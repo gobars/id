a1ee161ea168dc77d4a21898ce44f2ab3b271450
bingoohuang <bingoo.huang@gmail.com>
2020-07-27 16:29:29 +0800

    testcases

--- src/test/java/com/github/gobars/id/IdTest.java
+++ src/test/java/com/github/gobars/id/IdTest.java
@@ -10,17 +10,35 @@ import static org.junit.Assert.assertTrue;
 public class IdTest {
   @Test
   public void next() {
-    long id = Id.next();
-    System.out.println(id);
+    long[] ids =
+        new long[] {
+                Id.next(),
+          19789232541306880L,
+          19786145047138304L,
+          19785822010101760L,
+          19784150811590656L,
+          19783774090842112L,
+          19783674007306240L,
+          19783452816490496L,
+          19783351293362176L,
+          19782881404538880L,
+          19781203922014208L,
+          19780868970037248L,
+          19780425225256960L
+        };
 
-    Conf conf = Conf.fromSpec(SPEC);
-    //    long a = 346944917504L;
-    //    long a = 1782105694208L;
-    //    long a = 18825076141383680L;
-    long a = id;
+    for (long id : ids) {
+      System.out.println(id);
 
-    System.out.println("conf:" + conf);
-    System.out.println(conf.parseID(a));
+      Conf conf = Conf.fromSpec(SPEC);
+      //    long a = 346944917504L;
+      //    long a = 1782105694208L;
+      //    long a = 18825076141383680L;
+      long a = id;
+
+      System.out.println("conf:" + conf);
+      System.out.println(conf.parseID(a));
+    }
     assertTrue(Id.next() > 0);
   }
 
