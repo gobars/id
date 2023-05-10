3ff565e17b4f3c87e1530cb89f064e6371597c5c
label <916anan@gmail.com>
2023-05-10 17:42:52 +0800

    提交 test action 配置

--- scripts/docker-compose/up.sh
+++ scripts/docker-compose/up.sh
@@ -17,7 +17,7 @@ sleep 30s
 # Create the database and table
 docker exec -i dbs-mysql mysql -uroot -p'root' < ${DOCKER_COMPOSE_DIR}/init/mysql.sql
 docker exec -i dbs-postgres psql -U postgres < ${DOCKER_COMPOSE_DIR}/init/postgres.sql
-docker exec -i dbs-oracle11g /bin/bash /init/oracle/wait-for-oracle.sh
+docker exec -i dbs-oracle11g sh /init/oracle/wait-for-oracle.sh
 
 #docker exec -it dbs-mysql mysqladmin ping -uroot -proot --wait=30 --silent
 #docker exec -it dbs-mysql mysql -u root -p'root' -s -e "create database id;create table id.worker_id(id bigint auto_increment primary key,created datetime default current_timestamp,ip varchar(60),hostname varchar(60),pid int,reason varchar(60),biz varchar(60))engine=innodb default charset=utf8mb4;create table id.t_seq ( name        varchar(60) primary key comment '序列名称，不用的序列，请使用不用的名字', start       bigint   default 0     not null comment 'SEQ 起始值，用于重新循环', seq         bigint   default 0     not null comment '当前的SEQ取值', step  int default 10000 not null comment '步长', water_level int default 5000  not null comment '低于多少水位线需要补充', cycle  tinyint  default 0  not null comment '是否允许循环，必须max_seq同时设置时才生效', max_seq     bigint   default 0     not null comment '允许最大的序列值，0 时不校验', created     datetime default current_timestamp comment '创建时间', updated     datetime on update current_timestamp comment '更新时间' ) engine = innodb default charset = utf8mb4 comment '序列取号表';"
